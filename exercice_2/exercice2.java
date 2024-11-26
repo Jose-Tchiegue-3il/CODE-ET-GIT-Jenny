2b2ff9dbde6709293717f1bcf0df4a510e4844a3
Jose-Tchiegue-3il <tchieguj@3il.fr>
2024-11-26 18:18:50 +0100

    Modification des methodes dans exercice2

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,34 @@
+/**
+ * Classe permettant de gerer les notes des etudiants
+ */
 public class GestionnaireNotes {
-
-    // Affiche note et moyennes
+    /**
+     * 
+     */
+    
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+        
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
 
-        // Calcul de la somme des notes
+        CalculerMoyenne(notes);
+
+ 
+    /**
+     * 
+     */
+    CalculMoyenne(int[] notes) {
         int somme = 0;
         for (int note : notes) {
             somme += note;
         }
 
-        // Affichage de la moyenne
+        
         double moyenne = (double) somme / notes.length;
         System.out.println("Moyenne : " + moyenne);
+    }
+
     }
 }
